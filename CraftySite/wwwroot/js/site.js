54ba765c605f5e0ee16bacc47f9cfff34e6f25aa
Taylor Peters <93218689+taylor-peters@users.noreply.github.com>
2022-02-08 10:37:36 -0800

    Add project files.

---
+++ CraftySite/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿var id = null;
+
+function myMove() {
+    var elem = document.getElementById("animation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+
+    function frame() {
+        if (pos == 448) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.left = pos + 'px';
+        }
+    }
+}
